8fb2fd5bc57349f382aad995eda45a5d4005fd63
Gabriel-Rocha2000 <Gabriel_rocha2000@hotmail.com>
2025-06-16 21:47:27 -0300

    Feat material-ui-insert

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ['@emotion/styled'],
+  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
